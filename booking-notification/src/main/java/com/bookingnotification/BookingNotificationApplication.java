2aba757f44fdb9d509ecdfab6fc0a62151432b8f
Mateja Cosovic <mateja@192.168.1.8>
2023-06-05 21:41:19 +0200

    feat: inital setup

---
+++ booking-notification/src/main/java/com/bookingnotification/BookingNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.bookingnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingNotificationApplication.class, args);
+	}
+
+}
---
+++ booking-notification/src/test/java/com/bookingnotification/BookingNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
